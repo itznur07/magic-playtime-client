d149042bcc1e33c2d366dec1df29f2eb387bc669
Md Nuruddin <nuruddinalways99@gmail.com>
2023-05-18 20:06:09 +0600

    Navbar Updated

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
+import { FaBabyCarriage } from "react-icons/fa";
 import { Link } from "react-router-dom";
-import logo from "../../assets/logo.png";
 
 const Navbar = () => {
   return (
@@ -8,51 +8,45 @@ const Navbar = () => {
       <div className='bg-white relative z-50'>
         <nav className='md:flex md:flex-row flex flex-col items-center justify-between flex-wrap  py-6 md:mx-24'>
           {/* logo here */}
-          <div className='flex items-center flex-shrink-0 text-gray-500 mr-6'>
+          <div className='flex items-center  text-gray-500'>
             <Link to='/'>
-              <div className='flex items-center space-x-2 text-2xl'>
-                <img src={logo} className='w-16' alt='logo' />
-                <span>Magic Playtime</span>
+              <div className='flex items-center text-3xl space-x-2 text-[#e52165]'>
+                <FaBabyCarriage />
+                <span className='font-bold'>Magic Playtime</span>
               </div>
             </Link>
           </div>
           {/* navigation link here */}
-          <div className='md:flex md:flex-row flex flex-col items-center'>
+          <div className='md:flex md:flex-row md:text-md flex flex-col items-center space-x-5'>
             <Link
               to='/'
-              className='inline-block text-md px-4 py-2 leading-none   text-gray-800   hover:text-blue-500 hover:bg-white mt-4 lg:mt-0'
+              className='inline-block text-md px-4 py-2 leading-none  text-gray-800   hover:text-blue-500 hover:bg-slate-100 rounded-sm mt-4 lg:mt-0'
             >
               Home
             </Link>
             <Link
               to='/alltoys'
-              className='relative inline-block text-md px-4 py-2 leading-none  text-gray-800   hover:text-blue-500 hover:bg-white mt-4 lg:mt-0 ml-4'
+              className='relative inline-block text-md px-4 py-2 leading-none  text-gray-800   hover:text-blue-500 hover:bg-slate-100 rounded-sm mt-4 lg:mt-0 '
             >
               All Toys
             </Link>
             <Link
               to='/addtoys'
-              className='relative inline-block text-md px-4 py-2 leading-none  text-gray-800   hover:text-blue-500 hover:bg-white mt-4 lg:mt-0 ml-4'
+              className='relative inline-block text-md px-4 py-2 leading-none  text-gray-800   hover:text-blue-500 hover:bg-slate-100 rounded-sm mt-4 lg:mt-0 '
             >
               All Toys
             </Link>
             <Link
               to='/mytoys'
-              className='inline-block text-md px-4 py-2 leading-none  text-gray-800  hover:text-blue-500 hover:bg-white mt-4 lg:mt-0 ml-4'
+              className='inline-block text-md px-4 py-2 leading-none  text-gray-800  hover:text-blue-500 hover:bg-slate-100 rounded-sm  mt-4 lg:mt-0 '
             >
               My Toys
             </Link>
             <Link
               to='/blogs'
-              className='relative inline-block text-md px-4 py-2 leading-none  text-gray-800   hover:text-blue-500 hover:bg-white mt-4 lg:mt-0 ml-4'
+              className='relative inline-block text-md px-4 py-2 leading-none  text-gray-800   hover:text-blue-500 hover:bg-slate-100 rounded-sm mt-4 lg:mt-0'
             >
               Blogs
-            </Link>
-            <Link
-              to='/contact'
-              className='inline-block text-md px-4 py-2 leading-none  text-gray-800   hover:text-blue-500 hover:bg-white mt-4 lg:mt-0 ml-4'
-            >
-              Contact
             </Link>
           </div>
           {/* login & cart & wish icon here */}
@@ -67,7 +61,7 @@ const Navbar = () => {
                 </button> */}
 
               <Link to='/login'>
-                <button className='px-4 py-2.5 rounded bg-[#FF3811] text-white font-semibold'>
+                <button className='px-4 py-2.5 rounded bg-[#e52165] hover:bg-[#0d1137] text-white font-semibold'>
                   Log in
                 </button>
               </Link>
