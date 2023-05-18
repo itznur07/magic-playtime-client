d04c0eda2cd866f4326a192dd6777f49e11037bb
Md Nuruddin <nuruddinalways99@gmail.com>
2023-05-19 00:50:30 +0600

    Login page design

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,87 @@
+import { FaGoogle } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+function Login() {
+  const handleSignIn = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+
+    console.log({ email, password });
+  };
+
+  return (
+    <div className='flex justify-center space-x-24 items-center my-14'>
+      <form
+        onSubmit={handleSignIn}
+        className='bg-white py-14 px-28 rounded-lg border border-[#e52165]'
+      >
+        <h2 className='text-3xl text-center font-bold text-gray-800 mb-10'>
+          Login
+        </h2>
+
+        <div className='mb-4'>
+          <label htmlFor='email' className='block text-gray-700 font-bold mb-2'>
+            Email
+          </label>
+          <input
+            type='email'
+            name='email'
+            id='email'
+            className='w-full border rounded-lg py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-[#e52165] focus:border-transparent'
+          />
+        </div>
+        <div className='mb-2'>
+          <label
+            htmlFor='password'
+            className='block text-gray-700 font-bold mb-2'
+          >
+            Password
+          </label>
+          <input
+            type='password'
+            name='password'
+            id='password'
+            className='w-full border rounded-lg py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-[#e52165] focus:border-transparent'
+          />
+        </div>
+        <span className='text-sm mb-4'>
+          Forget password?{" "}
+          <span
+            className='text-blue-500 cursor-pointer'
+            // onClick={handleForgetPassword}
+          >
+            reset
+          </span>
+        </span>
+        <br />
+        <button
+          type='submit'
+          className='bg-[#ff3811] hover:bg-[#0d1137] text-white w-full font-bold py-2 mt-4 px-4 rounded focus:outline-none focus:shadow-outline'
+        >
+          Login
+        </button>
+        <button
+          type='button'
+          className='bg-red-600 hover:bg-[#0d1137] text-white w-full font-bold py-2 mt-4 px-4 rounded focus:outline-none focus:shadow-outline'
+        >
+          <div className='flex justify-center items-center space-x-2'>
+            <FaGoogle /> <span>Google</span>
+          </div>
+        </button>
+        <div className='text-center my-4 text-sm font-medium'>
+          or Sign in with
+        </div>
+        <div className='text-center text-md mt-3 text-sm text-slate-600'>
+          New here?{" "}
+          <Link to='/signup'>
+            <span className='text-[#ff3811] font-medium'>Sign up</span>
+          </Link>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
