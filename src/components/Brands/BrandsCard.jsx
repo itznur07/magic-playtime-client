3e6a852025367067b8eaa245eca9e06e3c198277
Md Nuruddin <nuruddinalways99@gmail.com>
2023-05-21 21:02:25 +0600

    Marquee slider added

--- src/components/Brands/BrandsCard.jsx
+++ src/components/Brands/BrandsCard.jsx
@@ -1,15 +1,18 @@
 import React from "react";
+import Marquee from "react-fast-marquee";
 
 const CompanyCard = ({ img, name }) => {
   return (
-    <div>
-      <img
-        className='w-32 rounded-full border cursor-pointer '
-        src={img}
-        alt='product-1'
-      />
-      <h1 className='text-center my-2 font-medium'>{name}</h1>
-    </div>
+    <Marquee>
+      <div>
+        <img
+          className='w-32 rounded-full cursor-pointer '
+          src={img}
+          alt='product-1'
+        />
+        <h1 className='text-center my-2 font-medium'>{name}</h1>
+      </div>
+    </Marquee>
   );
 };
 
