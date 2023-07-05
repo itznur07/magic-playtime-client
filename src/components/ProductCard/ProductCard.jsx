5546bbfb5460b334a2b90e854e0649348d03144c
Md Nuruddin <nuruddinalways99@gmail.com>
2023-07-05 21:28:51 +0600

    Update

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 
-function ProductCard({ images }) {
+function ProductCard({ images, title, price, discount_price }) {
   return (
     <div className='max-w-sm rounded overflow-hidden shadow-lg bg-blue-100'>
       <div className='relative'>
-        <img className='w-full' src={images[2]} alt='Toy airplane' />
+        <img
+          className='w-full object-cover h-56'
+          src={images[0]}
+          alt='Toy airplane'
+        />
         <span className='absolute top-0 left-0 bg-red-500 text-white px-2 py-1 text-xs font-bold uppercase'>
           Hot
         </span>
@@ -12,15 +16,33 @@ function ProductCard({ images }) {
           Sale
         </span>
       </div>
-      <div className='px-6 py-4'>
-        <div className='font-bold text-xl mb-2'>Rattles Blue Baby Toy</div>
+      <div className='px-2 mt-5'>
+        <div className='font-bold text-xl text-center'>
+          {title.slice(0, 16)}..
+        </div>
       </div>
-      <div className='px-6 pt-4 pb-2 flex justify-between items-center'>
-        <span className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2'>
-          $80.00
+      <div
+        className={`${
+          discount_price
+            ? "px-6 pt-4 pb-2 flex justify-between items-center"
+            : "flex justify-center items-center text-center pt-4 pb-2 "
+        }`}
+      >
+        <span
+          className={`${
+            discount_price
+              ? "inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 line-through"
+              : "bg-gray-200 rounded-full px-3 py-1 text-center text-sm font-semibold text-gray-700"
+          }`}
+        >
+          ${price}
         </span>
-        <span className='inline-block bg-green-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2'>
-          Save $20.00
+        <span
+          className={`inline-block ${
+            discount_price ? "bg-green-200" : ""
+          } rounded-full px-3 py-1 text-sm font-semibold  text-gray-700 `}
+        >
+          {discount_price ? `P: $${discount_price}` : null}
         </span>
       </div>
     </div>
